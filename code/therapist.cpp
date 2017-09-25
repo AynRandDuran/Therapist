68d5479a0b1242abfef1051ba7e5fbd3d1d170c6
pat <pletts@nmu.edu>
2017-09-25 00:22:28 -0400

    Moved BF functions to environment class

--- code/therapist.cpp
+++ code/therapist.cpp
@@ -62,60 +62,14 @@ string stripInvalid(string toStrip){
 	return validString;
 }
 
-int findLeftBracket(string source){
-	for(int i = 0; i < source.length(); i++){
-		if(source[i] == '[') return i;
-	}
-}
-
-int findRightBracket(string source){
-	for(int i = 0; i < source.length(); i++){
-		if(source[i] == ']') return i;
-	}
-}
-
 int main(int argc, char **argv){
 
 	//Load source code from a file and strip invalid characters
 	string sourceString = stripInvalid(loadSource(argv[1]).c_str());
 	cout << sourceString << endl;
+	
+	environment* test = new environment(30000, "+-<>,.[]");
+	test->process(sourceString);
 
-	int* array = new int[30000];
-	for(int i = 0; i < 30000; i++) array[i] = 0;
-	int DP = 0;
-
-	int leftBracket = findLeftBracket(sourceString);
-	int rightBracket = findRightBracket(sourceString);
-
-	//Outrageously basic interpreter, supports a single set of brackets to loop	
-	for(int iter = 0; iter < sourceString.length(); iter++){
-		switch(sourceString[iter]){
-			case '+':
-				array[DP]++;
-				break;
-			case '-':
-				array[DP]--;
-				break;
-			case '<':
-				DP--;
-				break;
-			case '>':
-				DP++;
-				break;
-			case ',':
-				cin >> array[DP];
-				break;
-			case '.':
-				cout << array[DP] << endl;
-				break;
-			case '[':
-				if(array[DP] == 0) iter = rightBracket-1;
-				break;
-			case ']':
-				if(array[DP] != 0)
-					iter = leftBracket;
-				break;
-		}
-	}
-
+	delete test;
 }
