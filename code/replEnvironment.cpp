9237d4f3bb49be4016eed4048b1f86a5bb9c5bf4
pat <nuggubler@gmail.com>
2017-11-19 17:52:59 -0500

    Can now recognize and expand predefined procedures

--- code/replEnvironment.cpp
+++ code/replEnvironment.cpp
@@ -1,16 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <unordered_map>
 #include "replEnvironment.h"
 #include "machine.h"
 using namespace std;
 
 replEnvironment::replEnvironment(bool AIO, bool signedCells, int tapeLength){
 	localMachine = new machine(tapeLength, signedCells, AIO, "");
+
+	bindings.insert({"pushRightOne", "[->+<]"}); //example procedure
+}
+
+char* replEnvironment::expandProcedure(char* statements){
+	unordered_map<string, string>::const_iterator result = bindings.find(statements);
+	if(result == bindings.end())
+		return statements;
+	else
+		return (char*)result->second.c_str();	
+}
+
+void replEnvironment::splitStatements(char* input){
+	int count = 0;
+	char* tmpStatement = strtok(input, ";");
+
+	while(tmpStatement){
+		tmpStatement = expandProcedure(tmpStatement);
+		statements[count] = tmpStatement;
+		tmpStatement = strtok(NULL, ";");
+		count++;
+	}
+	statements[count] = NULL;
 }
 
 bool replEnvironment::process(char* input){
-	localMachine->addToSource(input);
-	localMachine->processSource();
-	if(strchr(input, '.'))
-		printf("\n");
+	splitStatements(input);
+	int count = 0;
+	while(statements[count]){
+		localMachine->addToSource(statements[count]);
+		localMachine->processSource();
+		if(strchr(statements[count], '.'))
+			printf("\n");
+		count++;
+	}
 }
--- code/replEnvironment.h
+++ code/replEnvironment.h
@@ -1,14 +1,19 @@
 #ifndef __REPL__
 #define __REPL__
 #include "machine.h"
+#include <unordered_map>
 class replEnvironment{
 
 private:
 	machine* localMachine;
-	char* bindings[];
+	unordered_map<string, string> bindings;
+	char* statements[64];
+
 public:
 	replEnvironment(bool AIO, bool signedCells, int tapeLength);
+	char* expandProcedure(char* statements);
 	bool process(char* input);
+	void splitStatements(char* input);
 };
 
 #endif
