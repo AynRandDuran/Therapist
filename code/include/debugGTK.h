9104e67022092270649e0517a24d631b1934b080
pat <pletts@nmu.edu>
2017-12-01 12:35:52 -0500

    Debugger can draw and update tape frame, step and finish buttons work

--- code/include/debugGTK.h
+++ code/include/debugGTK.h
@@ -5,27 +5,37 @@
 #include <gtkmm/window.h>
 #include <gtkmm/grid.h>
 #include <gtkmm/frame.h>
+#include <gtkmm/label.h>
 #include "machine.h"
 
 class debugGTK : public Gtk::Window{
 private:
 	machine* BFM;
 
+	int windowWidth, windowHeight;
+
 	Gtk::Grid masterGrid;
 	Gtk::Frame controlFrame;
 	Gtk::Grid controlGrid;
+
+	Gtk::Frame tapeFrame;
 	Gtk::Grid tapeGrid;
+	Gtk::Label tapeLabels[10];
+
 	Gtk::Frame codeFrame;
 	Gtk::Grid stackGrid;
 	Gtk::Frame outputFrame;
 
 	Gtk::Button step;
 	Gtk::Button quit;
-	Gtk::Button advance;
+	Gtk::Button finish;
 
 public:
 	debugGTK();
 	void drawControlFrame();
+
+	void createTapeFrame();
+	void drawTapeFrame();
 
 	void stepF();
 	void advanceToEnd();
--- code/src/debugGTK.cpp
+++ code/src/debugGTK.cpp
@@ -5,25 +5,48 @@
 debugGTK::debugGTK()
 	:
 	step("Step"),
-	advance("Advance"),
+	finish("Finish"),
 	quit("Quit")
 {
-	BFM = new machine(30000, false, false, "+++.");
-
+	BFM = new machine(30000, false, false, "+++.>++++");
 	add(masterGrid);
 	masterGrid.set_border_width(5);
 	set_title("Therapist Debugger");
 	set_size_request(800, 500);
 
+	get_size(windowWidth, windowHeight);
 
 	masterGrid.attach(controlFrame, 1, 1, 1, 3);
+	masterGrid.attach(tapeFrame, 2, 1, windowWidth-1, 1);
 
 	drawControlFrame();
+	createTapeFrame();
 	show_all_children();
 
 	step.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::stepF));
-	advance.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::advanceToEnd));
+	finish.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::advanceToEnd));
 	quit.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::endDebugger));
+}
+
+void debugGTK::createTapeFrame(){
+	tapeFrame.set_border_width(10);
+	tapeFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
+	tapeFrame.set_label("Tape");
+	tapeFrame.add(tapeGrid);
+
+	for(int i = 0; i < 10; i++){
+		tapeLabels[i].set_margin_start(10);
+		tapeLabels[i].set_margin_end(10);
+		tapeGrid.attach(tapeLabels[i], i+1, 1, 1, 1);
+	}
+
+	drawTapeFrame();
+}
+
+void debugGTK::drawTapeFrame(){
+	for(int i = 0; i < 10; i++){
+		tapeLabels[i].set_text(std::to_string(BFM->getTapeAt(i)));
+	}
 }
 
 void debugGTK::drawControlFrame(){
@@ -32,12 +55,14 @@ void debugGTK::drawControlFrame(){
 	controlFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
 	controlFrame.set_label("Controls");
 	controlFrame.add(controlGrid);
+
 	controlGrid.set_border_width(5);
 	controlGrid.attach(step, 1, 1, 1, 1);
-	controlGrid.attach(advance, 1, 2, 1, 1);
+	controlGrid.attach(finish, 1, 2, 1, 1);
 	controlGrid.attach(quit, 1, 3, 1, 1);
+
 	step.set_border_width(5);
-	advance.set_border_width(5);
+	finish.set_border_width(5);
 	quit.set_border_width(5);
 }
 
@@ -45,10 +70,12 @@ debugGTK::~debugGTK(){}
 
 void debugGTK::stepF(){
 	BFM->processChar(1);
+	drawTapeFrame();
 }
 
 void debugGTK::advanceToEnd(){
 	BFM->processSource();
+	drawTapeFrame();
 }
 
 void debugGTK::endDebugger(){
