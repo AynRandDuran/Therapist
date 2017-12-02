210a87666b95cc3777173df0b8ec27f3c9baf28a
pat <pletts@nmu.edu>
2017-12-02 16:27:07 -0500

    GTK debugger implements machine as a subject to update tape now

---
+++ code/include/debugGTK.h
@@ -0,0 +1,50 @@
+#ifndef GTKMM_BF_DEBUG
+#define GTKMM_BF_DEBUG
+
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/grid.h>
+#include <gtkmm/frame.h>
+#include <gtkmm/label.h>
+#include "machine.h"
+
+class debugGTK : public Gtk::Window{
+private:
+	machine* BFM;
+
+	int windowWidth, windowHeight;
+
+	Gtk::Grid masterGrid;
+	Gtk::Frame controlFrame;
+	Gtk::Grid controlGrid;
+
+	Gtk::Frame tapeFrame;
+	Gtk::Grid tapeGrid;
+	Gtk::Label tapeCells[10];
+
+	Gtk::Frame codeFrame;
+	Gtk::Grid stackGrid;
+	Gtk::Frame outputFrame;
+
+	Gtk::Button step;
+	Gtk::Button advance;
+	Gtk::Button quit;
+	Gtk::Button finish;
+
+public:
+	debugGTK();
+	void drawControlFrame();
+
+	void createTapeFrame();
+	void drawTapeFrame();
+
+	void stepF();
+	void advanceToHalt();
+	void advanceToEnd();
+	void endDebugger();
+
+	virtual ~debugGTK();
+
+};
+
+#endif
---
+++ code/src/debugGTK.cpp
@@ -0,0 +1,94 @@
+#include "../include/debugGTK.h"
+#include "machine.cpp"
+#include <iostream>
+
+debugGTK::debugGTK()
+	:
+	step("Step"),
+	finish("Finish"),
+	quit("Quit"),
+	advance("Advance")
+{
+	BFM = new machine(30000, false, true, "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]@>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>@+.>++@.!");
+	add(masterGrid);
+	masterGrid.set_border_width(5);
+	set_title("Therapist Debugger");
+	set_size_request(800, 500);
+
+	get_size(windowWidth, windowHeight);
+
+	masterGrid.attach(controlFrame, 1, 1, 1, 3);
+	masterGrid.attach(tapeFrame, 2, 1, windowWidth-1, 1);
+
+	drawControlFrame();
+	createTapeFrame();
+	show_all_children();
+
+	step.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::stepF));
+	BFM->notify_tape_change().connect(sigc::mem_fun(*this, &debugGTK::drawTapeFrame));
+
+	advance.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::advanceToHalt));
+	finish.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::advanceToEnd));
+	quit.signal_clicked().connect(sigc::mem_fun(*this, &debugGTK::endDebugger));
+}
+
+void debugGTK::createTapeFrame(){
+	tapeFrame.set_border_width(10);
+	tapeFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
+	tapeFrame.set_label("Tape");
+	tapeFrame.add(tapeGrid);
+
+	for(int i = 0; i < 10; i++){
+		tapeCells[i].set_margin_start(10);
+		tapeCells[i].set_margin_end(10);
+		tapeCells[i].property_use_markup();
+		tapeGrid.attach(tapeCells[i], i+1, 1, 1, 1);
+	}
+
+	drawTapeFrame();
+}
+
+void debugGTK::drawTapeFrame(){
+	for(int i = 0; i < 10; i++){
+		tapeCells[i].set_text(std::to_string(BFM->getTapeAt(i)));
+	}
+}
+
+void debugGTK::drawControlFrame(){
+
+	controlFrame.set_border_width(10);
+	controlFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
+	controlFrame.set_label("Controls");
+	controlFrame.add(controlGrid);
+
+	controlGrid.set_border_width(5);
+	controlGrid.attach(step, 1, 1, 1, 1);
+	controlGrid.attach(advance, 1, 2, 1, 1);
+	controlGrid.attach(finish, 1, 3, 1, 1);
+	controlGrid.attach(quit, 1, 4, 1, 1);
+
+	step.set_border_width(5);
+	finish.set_border_width(5);
+	quit.set_border_width(5);
+}
+
+debugGTK::~debugGTK(){}
+
+void debugGTK::stepF(){
+	char retOp = BFM->processChar(1);
+}
+
+void debugGTK::advanceToEnd(){
+	BFM->processSource();
+	drawTapeFrame();
+}
+
+void debugGTK::advanceToHalt(){
+	char haltOp;
+	while(BFM->processChar(1) != '@');
+	drawTapeFrame();
+}
+
+void debugGTK::endDebugger(){
+	exit(0);
+}
